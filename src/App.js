325ff2ff4bdc807dcdb87c8764a704a52a8227f7
lequanglocdev <quanglocdev@gmail.com>
2023-06-08 23:33:04 +0700

    commit-edit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,36 @@
-import logo from './logo.svg';
-import './App.css';
 
+import './App.scss';
+import Header from './components/Header';
+import TableUsers from './components/TableUsers';
+import Container from 'react-bootstrap/Container';
+import { useState } from 'react';
+import { ToastContainer } from 'react-toastify';
 function App() {
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Hello world React
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <div className='App-container'>
+        <Header />
+        <Container>
+
+          <TableUsers />
+        </Container>
+
+      </div>
+      <ToastContainer
+        position="top-right"
+        autoClose={5000}
+        hideProgressBar={false}
+        newestOnTop={false}
+        closeOnClick
+        rtl={false}
+        pauseOnFocusLoss
+        draggable
+        pauseOnHover
+        theme="light"
+      />
+
+    </>
   );
 }
 
